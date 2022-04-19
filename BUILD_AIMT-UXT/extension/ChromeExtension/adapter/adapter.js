85e2401ee2a1328c04f3128d9fc7d4c2ffcb112c
Horusprg <62734053+Horusprg@users.noreply.github.com>
2022-04-19 19:32:20 -0300

    Stable online version

--- BUILD_AIMT-UXT/extension/ChromeExtension/adapter/adapter.js
+++
@@ -1,7 +0,0 @@
-const serverUrl = "https://t2uxtweb.azurewebsites.net";
-function post(endpoint, data) {
-  return fetch(serverUrl + "/" + endpoint, {
-    method: "POST",
-    body: JSON.stringify(data)
-  });
-}
--- viewer/DataAnalyzer/Version.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("16.1.75.0")]
-[assembly: AssemblyFileVersion("16.1.75.0")]
